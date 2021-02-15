2b4d6dc6272803b6c59d776d81d51a6f078839b8
teacarolina <teacarolina@icloud.com>
2021-02-15 11:25:00 +0100

    updated guestbook, you can now delete and edit messages

--- guestbookModify.php
+++ guestbookModify.php
@@ -1,3 +1,26 @@
+<?php
+    $dsn = "mysql:host=localhost;dbname=guestbook";
+    $user = "root";
+    $password = "";
+    $pdo = new PDO($dsn, $user, $password);
+
+    $action = $_GET['action'];
+
+    if($action == "update") {
+    $sql = "UPDATE entries SET message = :message_IN WHERE entries.Id = :id_IN";
+    $stm = $pdo->prepare($sql);
+    $stm->bindParam(":message_IN", $_POST['message']);
+    $stm->bindParam(":id_IN", $_POST['id']);
+    
+    if($stm->execute()) {
+        header("location:guestbook.php");
+    } else {
+        echo "Something went wrong";
+        die();
+    }
+};
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,17 +30,31 @@
     <title>Guestbook</title>
 </head>
 <body>
-    <h4>Edit your message 📑🖊️</h4>
-    <form action="guestbookModified.php" method="POST">
-    <label for="newMessage">Enter your message</label><br>
-    <textarea name="newMessage" cols="30" rows="10" value=""></textarea><br>
-    <input type="hidden" name="messageId" value="<?=$id?>">
-    <input type="submit" value="Post edited message">
-    </form>
-
     <?php
-    //not functioning yet, trying to solve
-    $id = $_GET['id'];
+    $dsn = "mysql:host=localhost;dbname=guestbook";
+    $user = "root";
+    $password = "";
+    $pdo = new PDO($dsn, $user, $password);
+
+    //collect data from db
+    $stm = $pdo->prepare("SELECT name, email, message, id FROM entries WHERE id=:id_IN");
+    $stm->bindParam(":id_IN", $_GET['id']);
+
+    $sucess = $stm->execute();
+    if(!$sucess) {
+        echo "<h3> Something went wrong </h3>";
+        die();
+    }
+
+    $userData = $stm->fetch();
+    
     ?>
+    <h4>Edit your message 📑🖊️</h4>
+    <form action="guestbookModify.php?action=update" method="POST">
+    <label for="message">Enter your message</label><br>
+    <textarea name="message" cols="30" rows="10"><?=$userData['message']?></textarea><br>
+    <input type="hidden" name="id" value="<?=$_GET['id']?>">
+    <input type="submit" name="submit" value="Post edited message">
+    </form>
 </body>
 </html>
